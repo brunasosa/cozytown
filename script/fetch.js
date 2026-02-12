de4cb94512e5a4a9a9d60a08f66e45ece4e98467
bruna <b.sofia.sosa@gmail.com>
2026-02-12 10:37:15 -0300

    update

--- script/fetch.js
+++ script/fetch.js
@@ -4,19 +4,32 @@ const container = document.getElementById("container");
 function showData(dataArray) {
   for (const item of dataArray) {
     container.innerHTML += `
+    <div class="forummessageContainer">
     <img src="" class="profilepicture" alt="">
-    <p> ${item.username}: ${item.message} </p>`;
+    <p> ${item.username}: ${item.message} </p>
+    </div>`;
   }
 }
-
 
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        console.log(data.general)
-        showData(data.general);
+        messages = data;
+        //console.log(data.general)
+        //showData(data.general);
     })
     .catch(error => {
         console.error("Error fetch", error);
     });
+
+    
+ function showMessages(forum){
+    for (const item of messages.forum) {
+    container.innerHTML += `
+    <div class="forummessageContainer">
+    <img src="" class="profilepicture" alt="">
+    <p> ${item.username}: ${item.message} </p>
+    </div>`;
+  }
+ }
 
--- script/navbar.js
+++ script/navbar.js
@@ -1,5 +1,6 @@
 navbar = document.getElementById("navbar");
 
 navbar.innerHTML += `
+    <img src="" class="profilepictureNavbar" alt="">
     <a href="forums.html">Forums</a>
     <a href="inbox.html">Inbox</a>`;
