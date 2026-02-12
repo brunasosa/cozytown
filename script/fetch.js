ecdd1da30b194de7b7784020f1d1babb905acad5
bruna <b.sofia.sosa@gmail.com>
2026-02-12 11:14:23 -0300

    update

--- script/fetch.js
+++ script/fetch.js
@@ -1,7 +1,7 @@
 const url = "json/forum_messages.json";
 const container = document.getElementById("container");
 
-function showData(dataArray) {
+function showMessages(dataArray) {
   for (const item of dataArray) {
     container.innerHTML += `
     <div class="forummessageContainer">
@@ -11,25 +11,28 @@ function showData(dataArray) {
   }
 }
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        messages = data;
-        //console.log(data.general)
-        //showData(data.general);
-    })
-    .catch(error => {
-        console.error("Error fetch", error);
-    });
+function getForumName(name) {
+  forumName = name;
+  console.log(forumName);
+}
 
-    
- function showMessages(forum){
-    for (const item of messages.forum) {
-    container.innerHTML += `
-    <div class="forummessageContainer">
-    <img src="" class="profilepicture" alt="">
-    <p> ${item.username}: ${item.message} </p>
-    </div>`;
-  }
- }
-
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    switch (forumName) {
+      case "general":
+        showMessages(data.general);
+        break;
+      case "pets":
+        showMessages(data.pets);
+        break;
+      case "fashion":
+        showMessages(data.fashion);
+        break;
+      default:
+        console.log("Error: Foro no existente");
+    }
+  })
+  .catch(error => {
+    console.error("Error fetch", error);
+  });
